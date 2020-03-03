31af50230bd841b84abfdcfd1a15709fb93341e1
samuelkyprianou <samuelkyprianou@gmail.com>
2020-03-03 16:36:22 +0000

    tracks

--- Trackster-Backend/app/controllers/users_controller.rb
+++ Trackster-Backend/app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
     def show
-        user = User.find_by(id: params[:id])
+        user = User.find_by(username: params[:id])
         render json: user
     end
 
